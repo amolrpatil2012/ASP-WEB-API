ad8989066e55b02b87be6e07ed79479fa2b32e54
amol <amolrpatil2012@gmail.com>
2025-01-28 09:29:18 +0530

    MVC

---
+++ MVC/MVC/Controllers/StudentsController1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        ApplicationDBContext dbContext;
+
+        public StudentsController ( ApplicationDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel model)
+        {
+            var student = new Student()
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Phone = model.Phone,
+                Subscribed = model.Subscribed
+            };
+            await dbContext.Students.AddAsync(student); 
+            await dbContext.SaveChangesAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var students = await dbContext.Students.ToListAsync();
+            return View(students);
+        }
+    }
+}
---
+++ MVC/MVC/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC.Models;
+
+namespace MVC.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext( DbContextOptions<ApplicationDBContext> options):base(options) { }
+
+        public DbSet<Student> Students { get; set; }    
+
+    }
+}
